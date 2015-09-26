3f17ea71ca3b9c70dbb8d1129f3ef5104b69328a
kuskova <basia.kuskova@gmail.com>
2015-09-26 18:47:46 +0300

    Task 3

--- task1.cpp
+++ task1.cpp
@@ -5,16 +5,17 @@
 
 using namespace std;
 
-void multiply(vector <vector <int> > a, vector <vector <int> > b, vector <vector <int> > &c){
-    c.resize(a.size());
-    for (int i = 0; i < a.size(); ++i){
-        c[i].resize(b[0].size());
-        for (int j = 0; j < b[0].size(); ++j){
+int** multiply(int** a, int** b, int n, int m, int k){ // a[n][k], b[k][m]
+    int** c=new int*[n];
+    for (int i = 0; i < n; ++i){
+        c[i]=new int[m];
+        for (int j = 0; j < m; ++j){
             c[i][j] = 0;
-            for (int l = 0; l < b.size(); ++l)
+            for (int l = 0; l < k; ++l)
                 c[i][j] += a[i][l] * b[l][j];
         }
     }
+    return c;
 }
 
 int main(){
---
+++ task3.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+inline void solve(int n, int a[], int m, int b[], int &n_sum, int sum[], int &n_pr, int pr[]){
+
+}
+
+inline void assert(int n, int a[], int m, int b[], int sum[], int pr[]){
+    int* sum_;
+    int* pr_;
+    int sz_sum, sz_pr;
+    solve(n, a, m, b, sz_sum, sum_, sz_pr, pr_);
+    if (sz_sum != max(n, m) || sz_pr != n + m - 1){
+        puts("WA");
+        return;
+    }
+    bool ok = true;
+    for (int i = 0; i < sz_sum; ++i)
+        ok &= (sum[i] == sum_[i]);
+    for (int i = 0; i < sz_pr; ++i)
+        ok &= (pr[i] == pr_[i]);
+    if (ok)
+        puts("OK");
+    else
+        puts("WA");
+}
+
+int main() {
+	int a1[2] = {3, 2}, b1[3] = {0, 4, 1};
+	int sum1[3] = {3, 6, 1}, pr1[4] = {0, 12, 11, 2};
+	assert(2, a1, 3, b1, sum1, pr1);
+    int a2[4] = {7, 0, -2, 3}, b2[4] = {2, -1, 0, -1};
+	int sum2[4] = {9, -1, -2, 2}, pr2[7] = {14, -7, -4, 1, -3, 2, -3};
+	assert(4, a2, 4, b2, sum2, pr2);
+	return 0;
+}
