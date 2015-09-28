1b5ce0e9f9e03055158db749e17ed726a4cd2e5a
VoynovDmitry <cry_miss@mail.ru>
2015-09-28 06:59:05 +0300

    fixed bug with overflow

--- task3.cpp
+++ task3.cpp
@@ -7,17 +7,16 @@ using namespace std;
 
 inline void solve(int n, int a[], int m, int b[], int &n_sum, int sum[], int &n_pr, int pr[]){
 
-int mem_n = n;
+int mem_n = n, na = n;
 	if (n!=m)
 		n_sum = max(n, m);
 	else
 	{
 		int mem_n = n;
-		while (a[n-1]+b[n-1]==0) n--;
-		n_sum = n;
-		n = mem_n;
+		while (a[na-1]+b[na-1]==0) na--;
+		n_sum = na;
 	}
-	for (int i=0;i < min (n, m);i++)
+	for (int i=0;i < min (na, m);i++)
 		sum[i] = a[i] + b[i];
 	if (n!=m)
 		if (n < m)
@@ -31,15 +30,10 @@ int mem_n = n;
 	}
 
 	n_pr = n + m - 1;
-	for (int i=0;i < n_pr;i++)
-	{
-		pr[i] = 0;
-		if (i>=n) a[i]=0;
-		if (i>=m) b[i]=0;
-	}
+	
 	for (int i=0;i < n_pr;i++)
 		for (int j=0;j <= i;j++)
-			pr[i] += a[j]*b[i-j];
+			if ((j<min(n, m))) pr[i] += a[j]*b[i-j];
 
 
 }
